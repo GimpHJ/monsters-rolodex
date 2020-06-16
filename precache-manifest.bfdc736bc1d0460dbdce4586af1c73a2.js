597b8cbfd9e5660b6ee4ecbff15bf42bcb84ea45
GimpHJ <devhjlim@gmail.com>
2020-06-16 01:06:17 -0700

    Updates

---
+++ precache-manifest.bfdc736bc1d0460dbdce4586af1c73a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1327b76d7dfaed0a1829dc3818ebb5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "87208f163138726214c6",
+    "url": "/monsters-rolodex/static/css/main.c33dbb04.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87208f163138726214c6",
+    "url": "/monsters-rolodex/static/js/main.eb58c2b7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
